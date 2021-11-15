f2c2c8a01a4bf9560f469f5f895690e0551dbfed
Khaldoun Ennahi <kennahi@e1r7p11.1337.ma>
2021-11-15 12:03:50 +0100

    firstfile

---
+++ get_next_line.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kennahi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/12 21:48:39 by kennahi           #+#    #+#             */
+/*   Updated: 2020/02/17 16:23:59 by kennahi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int			froo(char **s, int fd, char **line)
+{
+	*line = ft_strdup("");
+	free(s[fd]);
+	s[fd] = NULL;
+	return (0);
+}
+
+int			goot_nooxt_loone(char **s, int fd, char **line)
+{
+	char	*cup;
+	int		i;
+
+	i = 0;
+	while (s[fd][i] != '\n' && s[fd][i] != '\0')
+		i++;
+	if (s[fd][i] == '\n')
+	{
+		*line = ft_substr(s[fd], 0, i);
+		cup = ft_strdup(s[fd] + i + 1);
+		free(s[fd]);
+		s[fd] = cup;
+	}
+	else
+	{
+		*line = ft_strdup(s[fd]);
+		free(s[fd]);
+		s[fd] = NULL;
+		return (0);
+	}
+	return (1);
+}
+
+int			get_next_line(int fd, char **line)
+{
+	char		*buffer;
+	static char *s[4864];
+	int			rder;
+	char		*cup;
+
+	if (line == NULL || fd > 4864 || fd < 0 || read(fd, s, 0) < 0)
+		return (-1);
+	if (!(buffer = (char *)malloc(BUFFER_SIZE + 1)))
+		return (-1);
+	if (!s[fd])
+		s[fd] = ft_strdup("");
+	while ((rder = read(fd, buffer, BUFFER_SIZE)))
+	{
+		buffer[rder] = '\0';
+		cup = ft_strjoin(s[fd], buffer);
+		free(s[fd]);
+		s[fd] = cup;
+		if (ft_strchr(s[fd], '\n'))
+			break ;
+	}
+	free(buffer);
+	if (rder == 0 && *s[fd] == '\0')
+		return (froo(s, fd, line));
+	return (goot_nooxt_loone(s, fd, line));
+}
